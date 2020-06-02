2179160cc0fa42e5b18c035ee8047059745d4fec
Maria de los Angeles Paz Primero <maria.paz1116@uppuebla.edu.mx>
2020-06-01 22:34:31 -0500

    Excepciones Division

---
+++ divicionException/src/main/java/except.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class except {
+
+    public static void main(String[] args) {
+
+        int numerador;
+        Integer denominador = null;
+        float div;
+
+        numerador = 20;
+        denominador = 0;
+
+        System.out.println("Antes de la division");
+
+        try {
+            div = numerador / denominador;
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            div = 0;
+        }
+
+        System.out.println(div);
+        System.out.println("Despues de la division");
+    }
+
+}
